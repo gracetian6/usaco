4896e613f431dd561c8046ef9505ef79dae7df12
Grace Tian <37122992+gracetian6@users.noreply.github.com>
2019-07-09 10:42:06 -0500

    milk2 using objs

--- milk2.py
+++ milk2.py
@@ -4,6 +4,11 @@ LANG: PYTHON3
 TASK: milk2
 """
 
+class Milking:
+  def __init__(self, begin, end):
+    self.begin = begin
+    self.end = end
+
 fin = open ('milk2.in', 'r')
 fout = open ('milk2.out', 'w')
 rounds = int(fin.readline())
@@ -12,8 +17,8 @@ rounds = int(fin.readline())
 #read data
 times = []
 for i in range(rounds):
-    h, t = map(int, fin.readline().split())
-    times.append((h, t))
+    begin, end = map(int, fin.readline().split())
+    times.append(Milking(begin, end))
 
 #times = [(100, 200)]
 #times = [(300, 1000), (700, 1200), (1500, 2100)]
@@ -21,27 +26,32 @@ for i in range(rounds):
 #times = [(2, 3), (4, 5), (6, 7), (8, 9), (10, 11), (12, 13), (14, 15), (16, 17), (18, 19), (1, 20)]
 #times = [(100, 200), (200, 400), (400, 800), (800, 1600), (50, 100), (1700, 3200)]
 
-#sort times by head of every tuple
-times.sort(key=lambda tup: tup[0])
+#sort times by beginning of milking period
+times.sort(key=lambda tup: tup.begin)
 
+# walk over list, looking for long periods of times
+# tmilk = longest milking time
+# tnomilk = longest non-milking time
+# cur = current span of milking time being considered
+# milking = element traversed in list
 tmilk = 0
 tidle = 0
-h1, t1 = times[0]
-for h2, t2 in times:
-    # gap between h1,t1 and h2, t2
-    if h2 > t1:
-        t = h2 - t1
+cur = times[0]
+for milking in times:
+    # gap between cur and milking
+    if milking.begin > cur.end:
+        t = milking.begin - cur.end
         if t > tidle:
             tidle = t
-        t = t1 - h1
+        t = cur.end - cur.begin
         if t > tmilk:
             tmilk = t
-        h1, t1 = h2, t2
+        cur = milking
     # overlap between two intervals
-    elif t2 >= t1:
-        t1 = t2
+    elif milking.end > cur.end:
+        cur.end = milking.end
 # final milking period
-t = t1 - h1
+t = cur.end - cur.begin
 if t > tmilk:
     tmilk = t
 
@@ -49,7 +59,6 @@ if t > tmilk:
 print("Milk time: " + str(max_milk))
 print("Idle time: " + str(max_idle))
 """
-
 
 fout.write (str(tmilk) + ' ' + str(tidle) + '\n')
 fout.close()
