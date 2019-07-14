632979fb0d05c75b742b7059edf50bd2fde5e861
Grace Tian <37122992+gracetian6@users.noreply.github.com>
2019-07-14 17:42:17 -0500

    transform works

--- transform.py
+++ transform.py
@@ -5,7 +5,7 @@ TASK: transform
 """
 
 
-"""
+
 fin = open ('transform.in', 'r')
 fout = open ('transform.out', 'w')
 N = int(fin.readline())
@@ -14,21 +14,30 @@ N = int(fin.readline())
 #read data
 before = []
 after = []
-for i in range(N):
-    row = fin.readline().split()
-    before.append(row)
-for i in range(N):
-    row = fin.readline().split()
-    after.append(row)
-"""
-N = 3
-before = [['@', '-', '@'], ['-', '-', '-'], ['@', '@', '-']]
-after = [['@', '-', '@'], ['@', '-', '-'], ['-', '-', '@']]
+for _ in range(N):
+    line = list(fin.read(N))
+    before.append(line)
+    fin.readline()
+for _ in range(N):
+    line = list(fin.read(N))
+    after.append(line)
+    fin.readline()
 
-def rotate_90(sq):
+"""
+N = 5
+before = [['-', '@', '@', '@', '-'], ['-', '@', '@', '-', '-'], ['-', '@', '-', '-', '-']
+          ,['-', '-', '-', '-', '-'],['-', '-', '-', '-', '-']]
+after =  [['-', '-', '-', '-', '-'], ['-', '-', '-', '-', '@'], ['-', '-', '-', '@', '@']
+          ,['-','-', '@', '@', '@'], ['-', '-', '-', '-', '-']]
+"""
+print(N)
+print(before)
+print(after)
+
+def rotate(sq):
     sq_rotated = []
-    # ith row in sq_rotated is reversed ith column in sq
-    for n in range(len(sq)):
+    # nth row in sq_rotated = reversed nth column in sq
+    for n in range(N):
         # nth column in sq becomes ith row in sq_rotated
         sq_col_n = [] # nth col of sq
         # traverse sq
@@ -38,11 +47,33 @@ def rotate_90(sq):
         sq_rotated.append(sq_col_n)
     return sq_rotated
 
-print(before)
-print(rotate_90(before))
-print(after)
+def reflect(sq):
+    sq_refl = []
+    # nth row in sq_reflected = reversed nth row in sq
+    for n in range(N):
+        sq[n].reverse()
+        sq_refl.append(sq[n])
+    return sq_refl
 
-"""
-fout.write ('output \n')
+
+# times = # of times changed
+# cur_sq = changed sq
+# sq = original sq
+def check_transf(sq, cur_sq, times):
+    # base cases
+    if times > 0 and cur_sq == after:
+        if times <= 4:
+            return times
+        else:
+            return 5
+    elif times >= 8:
+        return 7
+    # reflect sq
+    elif times == 3:
+        return check_transf(sq, reflect(sq), times + 1)
+    # rotate sq
+    elif times <= 2 or times >= 4:
+        return check_transf(sq, rotate(cur_sq), times + 1)
+
+fout.write(str(check_transf(before, before, 0)) + '\n')
 fout.close()
-"""
